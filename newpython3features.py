711818eee98c0ae18e840ea835a7aca14a6cae2f
Paul Borawski <paulborawski14@gmail.com>
2020-02-23 09:27:30 -0500

    python3 features

--- newpython3features.py
+++ newpython3features.py
@@ -1,1 +1,14 @@
-formatted strings - (f'hello {paul}')
+1. print is a function
+
+2. formatted strings - (f'hello {paul}')
+
+3. Unpacking
+# Can use *misc
+# python 2
+person = (&quot;Steve&quot;, &quot;Hammond&quot;, 34, &quot;England&quot;, &quot;spaces&quot;)
+name, surname, age, country, indent_pref = person
+# python 3
+person = (&quot;Steve&quot;, &quot;Hammond&quot;, 34, &quot;England&quot;, &quot;spaces&quot;)
+name, *misc, indent_pref = person
+
+4. Enums
